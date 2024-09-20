768ca147a9cbd56a15f83d36b9483f838a5da20e
Daniel Uribe <a.22308051281323@cbtis128.edu.mx>
2024-09-20 09:32:11 -0600

    Add files via upload

---
+++ operadoresA.py
@@ -0,0 +1,60 @@
+print("Act 9 clases v2")
+print("Daniel Uribe    Mat: 22308051281323")
+# zona de clases
+class Operadores1323:
+    #zona de funciones
+    #suma
+    def suma1323(self, D, U):
+        s1323=D+U
+        print(f"la suma de {D} + {U} = {s1323}")
+    #resta
+    def resta1323(self,D,U):
+        r1323=D-U
+        print(f" la resta de {D} - {U} = {r1323}")
+    #multiplicacion
+    def multiplicacion1323(self,D,U):
+        m1323=D*U
+        print(f" la multiplicacion de {D} * {U} = {m1323}")
+    #division
+    def division1323(self,D,U):
+        d1323=D/U
+        print(f" la division de {D} / {U} = {d1323}")
+    #modulo
+    def modulo1323(self,D,U):
+        mo1323=D%U
+        print(f" el modulo de {D} % {U} = {mo1323}")
+    #exponente
+    def exponente1323(self,D,U):
+        e1323=D**U
+        print(f" el exponente de {D} ** {U} = {e1323}")
+    #cociente
+    def cociente1323(self,D,U):
+        c1323=D//U
+        print(f" el cociente de {D} // {U} = {c1323}")
+
+# zona de creacion del objeto
+operauribe=Operadores1323()
+
+# zona de uso de objetos
+print(" Funcion Suma")
+operauribe.suma1323(3,6)
+print("")
+print(" Funcion Resta")
+operauribe.resta1323(3,6)
+print("")
+print(" Funcion Multiplicacion")
+operauribe.multiplicacion1323(3,6)
+print("")
+print(" Funcion Division")
+operauribe.division1323(3,6)
+print("")
+print(" Funcion Modulo")
+operauribe.modulo1323(3,6)
+print("")
+print(" Funcion Exponente")
+operauribe.exponente1323(3,6)
+print("")
+print(" Funcion Cociente")
+operauribe.cociente1323(3,6)
+print("")
+
